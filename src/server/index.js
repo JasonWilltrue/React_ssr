f486458ffd8fe00b76a0f2eb2307408849f38e37
lovewcc <148373644@qq.com>
2018-12-28 09:26:42 +0800

    小调整

--- src/server/index.js
+++ src/server/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 const app = express();
 
 app.use(express.static('public')); //让浏览器识别静态文件
-import { render } from './util';
-// const content = renderToString(<Home />);
 
+import { render } from './util';
+
+// const content = renderToString(<Home />);
 app.get('*', function(req, res) {
 	res.send(render(req));
 });
 var server = app.listen(3000);
+
 console.log('服务器3000开启了');
